3286fed818e8be7ce7923deac2f5946125d12b34
Lane Shetron <laneshetron@stockx.com>
2019-03-15 19:00:27 -0400

    Genericize heap nodes

--- orderbook.go
+++ orderbook.go
@@ -2,11 +2,34 @@ package orderbook
 
 import "container/heap"
 
+type Item interface {
+	Peek() *Order
+}
+
+type Node struct {
+	Item
+	Key    string
+	weight float64
+	index  int
+}
+
+func NewNode(key string, i Item, weight float64) Node {
+	return Node{
+		Item:   i,
+		Key:    key,
+		weight: weight,
+	}
+}
+
 type Order struct {
 	Price    float64 `json:"price"`
 	Quantity float64 `json:"quantity"`
 	OrderId  string  `json:"orderId"`
-	index    int
+	Country  string  `json:"country"`
+}
+
+func (o *Order) Peek() *Order {
+	return o
 }
 
 type Quote struct {
@@ -19,21 +42,21 @@ type TradeEvent struct {
 	Quantity float64
 }
 
-type BaseHeap []*Order
+type BaseHeap []*Node
 type AskOrders struct {
 	BaseHeap
 }
 type BidOrders struct {
 	BaseHeap
 }
-type OrdersMap map[string]*Order
+type OrdersMap map[string]*Node
 
 func (ob AskOrders) Less(i, j int) bool {
-	return ob.BaseHeap[i].Price < ob.BaseHeap[j].Price
+	return ob.BaseHeap[i].Peek().Price < ob.BaseHeap[j].Peek().Price
 }
 
 func (ob BidOrders) Less(i, j int) bool {
-	return ob.BaseHeap[i].Price > ob.BaseHeap[j].Price
+	return ob.BaseHeap[i].Peek().Price > ob.BaseHeap[j].Peek().Price
 }
 
 func (h BaseHeap) Len() int { return len(h) }
@@ -45,7 +68,7 @@ func (h BaseHeap) Swap(i, j int) {
 }
 
 func (h *BaseHeap) Push(x interface{}) {
-	*h = append(*h, x.(*Order))
+	*h = append(*h, x.(*Node))
 	(*h)[len(*h)-1].index = len(*h) - 1
 }
 
@@ -62,36 +85,35 @@ type BidBook struct {
 
 func (bb *BidBook) Peek() *Order {
 	if bb.Orders.Len() > 0 {
-		return bb.Orders.BaseHeap[0]
+		return bb.Orders.BaseHeap[0].Peek()
 	} else {
 		return nil
 	}
 }
 
-func (bb *BidBook) Push(orderId string, price float64, amount float64) {
-	bb.Remove(orderId) // ensure orderId does not already exist
-	order := Order{Price: price, Quantity: amount, OrderId: orderId}
-	heap.Push(&bb.Orders, &order)
-	bb.OrdersMap[orderId] = &order
+func (bb *BidBook) Push(n *Node) {
+	bb.Remove(n.Key) // ensure Key does not already exist
+	heap.Push(&bb.Orders, n)
+	bb.OrdersMap[n.Key] = n
 }
 
-func (bb *BidBook) Pop() *Order {
-	order := heap.Pop(&bb.Orders).(*Order)
-	delete(bb.OrdersMap, order.OrderId)
-	return order
+func (bb *BidBook) Pop() *Node {
+	node := heap.Pop(&bb.Orders).(*Node)
+	delete(bb.OrdersMap, node.Key)
+	return node
 }
 
-func (bb *BidBook) Remove(orderId string) {
-	if _, ok := bb.OrdersMap[orderId]; ok {
-		heap.Remove(&bb.Orders, bb.OrdersMap[orderId].index)
-		delete(bb.OrdersMap, orderId)
+func (bb *BidBook) Remove(key string) {
+	if _, ok := bb.OrdersMap[key]; ok {
+		heap.Remove(&bb.Orders, bb.OrdersMap[key].index)
+		delete(bb.OrdersMap, key)
 	}
 }
 
 func (bb *BidBook) volume() float64 {
 	var total float64 = 0
-	for _, order := range bb.Orders.BaseHeap {
-		total += order.Quantity
+	for _, node := range bb.Orders.BaseHeap {
+		total += node.Peek().Quantity
 	}
 	return total
 }
@@ -103,36 +125,35 @@ type AskBook struct {
 
 func (ab *AskBook) Peek() *Order {
 	if ab.Orders.Len() > 0 {
-		return ab.Orders.BaseHeap[0]
+		return ab.Orders.BaseHeap[0].Peek()
 	} else {
 		return nil
 	}
 }
 
-func (ab *AskBook) Push(orderId string, price float64, amount float64) {
-	ab.Remove(orderId) // ensure orderId does not already exist
-	order := Order{Price: price, Quantity: amount, OrderId: orderId}
-	heap.Push(&ab.Orders, &order)
-	ab.OrdersMap[orderId] = &order
+func (ab *AskBook) Push(n *Node) {
+	ab.Remove(n.Key) // ensure Key does not already exist
+	heap.Push(&ab.Orders, n)
+	ab.OrdersMap[n.Key] = n
 }
 
-func (ab *AskBook) Pop() *Order {
-	order := heap.Pop(&ab.Orders).(*Order)
-	delete(ab.OrdersMap, order.OrderId)
-	return order
+func (ab *AskBook) Pop() *Node {
+	node := heap.Pop(&ab.Orders).(*Node)
+	delete(ab.OrdersMap, node.Key)
+	return node
 }
 
-func (ab *AskBook) Remove(orderId string) {
-	if _, ok := ab.OrdersMap[orderId]; ok {
-		heap.Remove(&ab.Orders, ab.OrdersMap[orderId].index)
-		delete(ab.OrdersMap, orderId)
+func (ab *AskBook) Remove(key string) {
+	if _, ok := ab.OrdersMap[key]; ok {
+		heap.Remove(&ab.Orders, ab.OrdersMap[key].index)
+		delete(ab.OrdersMap, key)
 	}
 }
 
 func (ab *AskBook) volume() float64 {
 	var total float64 = 0
-	for _, order := range ab.Orders.BaseHeap {
-		total += order.Quantity
+	for _, node := range ab.Orders.BaseHeap {
+		total += node.Peek().Quantity
 	}
 	return total
 }
